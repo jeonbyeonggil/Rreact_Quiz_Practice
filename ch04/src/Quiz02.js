9fbaf1abeb3f6e597ee67cbcea1c4d8f06450ea1
Jeonbyeonggil <plkr288@naver.com>
2024-11-01 10:39:47 +0900

    first commit

---
+++ ch04/src/Quiz02.js
@@ -0,0 +1,44 @@
+import React, { useState } from 'react'
+
+function Quiz02() {
+   const [form, setForm] = useState({
+      username: '',
+      message: '',
+   })
+
+   const { username, message } = form
+
+   const onChange = (e) => {
+      // 이 부분 작성
+      const name = e.target.name
+      const value = e.target.value
+
+      // name"message"인 input창에서 입력한 값이 20자를 넘으면 alert창을 띄움.
+      if (name === 'message' && value.length > 20) {
+         alert('20자를 초과해 작성 할 수 없습니다!')
+         return
+      }
+   }
+
+   const onClick = () => {
+      alert(`입력값: ${username}, ${message}`)
+      setForm({ username: '', message: '' })
+   }
+
+   const onKeyDown = (e) => {
+      if (e.key === 'Enter') {
+         onClick()
+      }
+   }
+
+   return (
+      <div>
+         <h1>이벤트 연습</h1>
+         <input type="text" name="username" placeholder="이름 입력" value={username} onChange={onChange} />
+         <input type="text" name="message" placeholder="메시지 입력 (최대 20자)" value={message} onChange={onChange} onKeyDown={onKeyDown} />
+         <button onClick={onClick}>확인</button>
+      </div>
+   )
+}
+
+export default Quiz02
